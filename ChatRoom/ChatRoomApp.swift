fe4f3a9dde80b19c8037b29469247fe6e8caf760
reassign <snowboardz125@gmail.com>
2023-08-03 12:55:06 -0400

    Initial Commit

---
+++ ChatRoom/ChatRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatRoomApp.swift
+//  ChatRoom
+//
+//  Created by Alex Mierzejewski on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
